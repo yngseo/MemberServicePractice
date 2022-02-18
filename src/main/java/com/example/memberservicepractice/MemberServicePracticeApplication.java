d7fdf8f14365b2e8194be493132ada59188befe0
yngseo <an_dyouu@naver.com>
2022-02-18 16:00:15 +0900

    Initial commit

---
+++ src/main/java/com/example/memberservicepractice/MemberServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.memberservicepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServicePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberServicePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/memberservicepractice/MemberServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memberservicepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServicePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
